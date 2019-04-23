201e1c571ee5e851d03f8d66a68fdd426630538d
Psyk0 <onunvieira@gmail.com>
2019-04-23 20:41:26 +0100

    Interface Tweaks
    Tweaks and State Machine

--- DestinationEarth/src/logicaJogo/Game.java
+++ DestinationEarth/src/logicaJogo/Game.java
@@ -1,16 +1,37 @@
 package logicaJogo;
-import java.util.*;
 
-public class Game{
+import java.util.*;
+import logicaEstados.*;
+
+public class Game {
+
     private GameData gameData; //Dados do jogo
-    
-    public Game(){
+    private IStates state;
+
+    public Game() {
         gameData = new GameData(); //Novo jogo
+        state = new AwaitBeginning(gameData);
     }
+
     public GameData getGameData() {
         return gameData;
-    }		
+    }
+
     public void setGameData(GameData gameData) {
         this.gameData = gameData;
+    }
+
+    public IStates getState() {
+        return state;
+    }
+
+    public void setState(IStates state) {
+        this.state = state;
+    }
+
+    //--------------Estados--------------
+    //AwaitBeginning
+    public void resolveStart() {
+        setState(getState().start());
     }
 }
