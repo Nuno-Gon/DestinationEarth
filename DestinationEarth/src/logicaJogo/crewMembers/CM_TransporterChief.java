f730881ef1cff2e87b6dadfb64317a352321db1e
joaoparis <49085010+joaoparis@users.noreply.github.com>
2019-06-04 20:04:03 +0100

    f

--- DestinationEarth/src/logicaJogo/crewMembers/CM_TransporterChief.java
+++ DestinationEarth/src/logicaJogo/crewMembers/CM_TransporterChief.java
@@ -7,6 +7,7 @@ public class CM_TransporterChief extends CrewMember {
     public CM_TransporterChief(int n) {
         super(n);
         infoSpecial = "\nCan teleport to any room on the ship for 1 Action Point.\n";
+        name = "Transporter Chief";
     }
 
     @Override
